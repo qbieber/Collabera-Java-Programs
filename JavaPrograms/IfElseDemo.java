687459a074d71f5272428f8fc9cd189db5c2d6ae
qbieber <qbieber@hotmail.com>
2019-08-07 15:54:23 -0400

    Adding Java code

---
+++ JavaPrograms/IfElseDemo.java
@@ -0,0 +1,20 @@
+public class IfElseDemo
+{
+    public static void main(String[] args) 
+    {
+        int x = 16, y = 16;
+        
+        if (x < y)
+        {
+            System.out.println("x is less than y");
+        }
+        else if (x == y)
+        {
+            System.out.println("x is equal to y");
+        }
+        else
+        {
+            System.out.println("x is greater than y"); 
+        }
+    }
+}
---
+++ JavaPrograms/IfTest.java
@@ -0,0 +1,10 @@
+public class IfTest
+{
+    public static void main(String[] args)
+    {
+        if (6 > 3)
+        {
+            System.out.println("6 is greater than 3");
+        }
+    }  
+}
